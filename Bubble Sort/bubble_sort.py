3a1a44f0b4d4ad13088698d61ca1dac070f7f926
MadeYoga <vngla21@gmail.com>
2021-04-25 13:15:55 +0700

    Update bubble sort py to match other solution

--- Bubble Sort/bubble_sort.py
+++ Bubble Sort/bubble_sort.py
@@ -4,9 +4,9 @@ from random import shuffle
 
 def bubble_sort(array):
     for i in range(len(array)):
-        for j in range(len(array) - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
+        for j in range(i, len(array)):
+            if array[i] > array[j]:
+                array[j], array[i] = array[i], array[j]
 
     return array
 
