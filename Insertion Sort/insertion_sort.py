4dc2bcc164fc65b99ff4affe268f1a58f9144187
MadeYoga <vngla21@gmail.com>
2021-04-26 11:47:44 +0700

    Add insertion sort in Python

---
+++ Insertion Sort/insertion_sort.py
@@ -0,0 +1,25 @@
+import numpy as np
+from random import shuffle
+
+
+def insertion_sort(array):
+    for i in range(len(array)):
+        current_key = array[i]
+        for j in range(i - 1, -1, -1):
+            if current_key < array[j]:
+                array[j + 1], array[j] = array[j], array[j + 1]
+                continue
+            break
+    return array
+
+
+# Buat array berisi 0 sampai 999
+random_array = np.arange(1000)
+
+# Acak isi array
+shuffle(random_array)
+
+print(random_array)
+
+print(insertion_sort(random_array))
+
